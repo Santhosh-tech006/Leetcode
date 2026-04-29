7d91c4231c447417acb9114a450a8afc438a718c
Santhosh-tech006 <aidssanthosh@gmail.com>
2026-04-29 21:26:10 +0530

    Time: 35 ms (70.47%) | Memory: 14.7 MB (46.52%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        prefix_sum = 0
+        count = 0
+        prefix_map = {0: 1}  # base case
+
+        for num in nums:
+            prefix_sum += num
+
+            if (prefix_sum - k) in prefix_map:
+                count += prefix_map[prefix_sum - k]
+
+            prefix_map[prefix_sum] = prefix_map.get(prefix_sum, 0) + 1
+
+        return count
