59b6de28c7f5a8ceccc87e8d25c1c97108368867
Santhosh-tech006 <aidssanthosh@gmail.com>
2026-04-29 21:43:02 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (90.14%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        prev1 = 0  # max money till previous house
+        prev2 = 0  # max money till house before previous
+
+        for num in nums:
+            temp = max(prev1, prev2 + num)
+            prev2 = prev1
+            prev1 = temp
+
+        return prev1
